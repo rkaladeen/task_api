75418e1a14785504b50d6ee7b090e141a5fd639f
raj-kaladeen <r.kaladeen@gmail.com>
2019-08-15 19:17:31 -0400

    redesign ui and began form

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  title = 'Welcome to Tasks';
+  tasks: any = "";
+
+  constructor(private _httpService: HttpService) { }
+
+  ngOnInit() {
+    let observable = this._httpService.getAll();
+    observable.subscribe(data => {
+      this.tasks = data;
+    })
+  }
+
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
-import { AppRoutingModule } from './app-routing.module';
+
+import { HttpService } from './http.service';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, AngularFontAwesomeModule],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
